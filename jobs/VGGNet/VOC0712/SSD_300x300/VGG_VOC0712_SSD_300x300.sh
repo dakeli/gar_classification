cef0e4cf0a440714aa2b45c2fa3053754a228075
jindong.li <jindong.li@iluvatar.ai>
2019-07-20 20:42:48 +0800

    edit

--- jobs/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300.sh
+++
@@ -1,5 +0,0 @@
-cd /home/dake/code/caffe-ssd-1
-./build/tools/caffe train \
---solver="models/VGGNet/VOC0712/SSD_300x300/solver.prototxt" \
---snapshot="models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_16603.solverstate" \
---gpu 0 2>&1 | tee jobs/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300.log
